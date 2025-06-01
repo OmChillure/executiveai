7a66d5f6a8a453eba72508eff172c52cdcf5980b
omchillure <omchillure4@gmail.com>
2025-06-02 01:47:04 +0530

    header ui fix

--- frontend/src/components/imageLayer.tsx
+++ frontend/src/components/imageLayer.tsx
@@ -14,16 +14,11 @@ const ImageOverlay: React.FC<ImageOverlayProps> = ({ imageSrc }) => {
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         <div className="flex flex-col items-center text-center">
-          <span className="text-[rgba(151,59,42,1)] text-[6px] md:text-[8px] lg:text-[10px]">(w3</span>
-          <p className="text-[rgba(162,116,106,1)] text-[10px] md:text-sm lg:text-base font-normal leading-relaxed mt-1">
-            You turn your career
+          <p className="text-[#ad9894] text-[14px] md:text-sm lg:text-lg font-normal leading-relaxed mt-1">
+            Turn your career
             <br />
-            into a calling.
+            into prompting.
           </p>
-          <p className="text-[rgba(37,35,33,1)] text-[11px] md:text-sm lg:text-base mt-12 md:mt-16 lg:mt-20">
-            You contribute
-          </p>
-          <p className="text-[rgba(24,24,22,1)] text-[8px] md:text-xs lg:text-sm mt-2">more to society.</p>
         </div>
       </div>
     </div>
